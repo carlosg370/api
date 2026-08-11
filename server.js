51862b1a50e5706ac2c54d6711e185abe694ac40
carlosg370 <carlosgui2009@gmail.com>
2026-08-11 10:00:06 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-const express = require('express')
+const express = require ('express');
 
-const app = express();
-app.use(express.json());
-app.get('/alunos',(reg, res)=>{ 
-    res.send("lendo dados do banco...")
+const app = express ();
+    app.use(express.json());
+
+app.get('/alunos', (req, res)=> {
+
+    res.send ("lendo dados do banco")
 })
-app.listen(3000,() =>{ 
-    console.log =('Servidor rodando na porta 3000');
+
+
+app.post('/alunos', (req, res) => {
+const novoItem = { id: idCounter++, nome: req.body.nome} ;
+ itens.push(novoItem);
+ res.status(201).json(novoItem);              
+
+
 });
 
+
+
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+});
