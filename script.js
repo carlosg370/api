8ee9eda3aacdca6c4d96650f814581988bee8ff6
carlosg370 <carlosgui2009@gmail.com>
2026-08-11 12:25:29 -0300

    Add files via upload
    app

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputUpload = document.getElementById('meu-upload')
+
+inputUpload.addEventListener('change', function(evento) {  
+
+    const arquivo = evento.target.files[0]
+
+    if (arquivo) {
+        const nomeDaImagem = arquivo.nomeDaImagem
+        console.log(nomeDaImagem)
+    } else {
+        console.log("Nenhum arquivo selecionado.");
+    }
+});
